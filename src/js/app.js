d5053378a6d387f0bb7ac1f48cfdc1025f41cdc1
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2019-06-23 22:21:14 -0300

    0.4

--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,17 @@ app.controller("TreeController", ["$scope", function($scope) {
 
 app.controller("ProductsListController", ["$scope", function($scope){
     $scope.productItem = [
-        {completed: true, itemName: "Campo Confecções", code: 001},
-        {completed: true, itemName: "Bermudas", code: 002},
-        {completed: true, itemName: "Calças", code: 003},
-        {completed: true, itemName: "Sarja (m)", code: 004},
-        {completed: true, itemName: "Social", code: 005},
-        {completed: true, itemName: "Camisas", code: 006},
-        {completed: true, itemName: "Esporte", code: 007},
-        {completed: true, itemName: "Gola Polo", code: 008},
-        {completed: true, itemName: "Grife", code: 009},
-        {completed: true, itemName: "Lacoste", code: 010},
-        {completed: true, itemName: "Social", code: 011},
-        {completed: true, itemName: "Terno", code: 012},
+        {id: 01, completed: true, itemName: "Campo Confecções", obs: "Todos os itens"},
+        {id: 02, completed: true, itemName: "Bermudas", obs: "Todos os itens"},
+        {id: 03, completed: true, itemName: "Calças", obs: "Todos os itens"},
+        {id: 04, completed: true, itemName: "Sarja (m)", obs: "Todos os itens"},
+        {id: 05, completed: true, itemName: "Social", obs: "Todos os itens"},
+        {id: 06, completed: true, itemName: "Camisas", obs: "Todos os itens"},
+        {id: 07, completed: true, itemName: "Esporte", obs: "Todos os itens"},
+        {id: 08, completed: true, itemName: "Gola Polo", obs: "Todos os itens"},
+        {id: 09, completed: true, itemName: "Grife", obs: "Todos os itens"},
+        {id: 10, completed: true, itemName: "Lacoste", obs: "Todos os itens"},
+        {id: 11, completed: true, itemName: "Social", obs: "Todos os itens"},
+        {id: 12, completed: true, itemName: "Terno", obs: "Todos os itens"},
     ]
 }]);
