0661a108a5f2bba72fc8568ffbfb32d113408cbd
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2019-06-26 23:21:46 -0300

    teste

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,36 @@
- var app = angular.module('treasyChallenge', []);
+ var app = angular.module('treasyChallenge', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider
+        .otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'view/products.html'
+        })
+        .state('products', {
+            url: 'view/products',
+            templateUrl: 'products.html'
+        });
+});
 
 app.controller("TreeController", ["$scope", function($scope) {
-    $scope.appTitle = "Produtos";
+    $scope.appTitle = "Produtos";    
 }]);
 
 app.controller("ProductsListController", ["$scope", function($scope){
     $scope.productItem = [
-        {id: 01, completed: true, itemName: "Campo Confecções", obs: "Todos os itens"},
-        {id: 02, completed: true, itemName: "Bermudas", obs: "Todos os itens"},
-        {id: 03, completed: true, itemName: "Calças", obs: "Todos os itens"},
-        {id: 04, completed: true, itemName: "Sarja (m)", obs: "Todos os itens"},
-        {id: 05, completed: true, itemName: "Social", obs: "Todos os itens"},
-        {id: 06, completed: true, itemName: "Camisas", obs: "Todos os itens"},
-        {id: 07, completed: true, itemName: "Esporte", obs: "Todos os itens"},
-        {id: 08, completed: true, itemName: "Gola Polo", obs: "Todos os itens"},
-        {id: 09, completed: true, itemName: "Grife", obs: "Todos os itens"},
-        {id: 10, completed: true, itemName: "Lacoste", obs: "Todos os itens"},
-        {id: 11, completed: true, itemName: "Social", obs: "Todos os itens"},
-        {id: 12, completed: true, itemName: "Terno", obs: "Todos os itens"},
+        {id: 01, itemName: "Campo Confecções", obs: "Todos os itens"},
+        {id: 02, itemName: "Bermudas", obs: "Todos os itens"},
+        {id: 03, itemName: "Calças", obs: "Todos os itens"},
+        {id: 04, itemName: "Sarja (m)", obs: "Todos os itens"},
+        {id: 05, itemName: "Social", obs: "Todos os itens"},
+        {id: 06, itemName: "Camisas", obs: "Todos os itens"},
+        {id: 07, itemName: "Esporte", obs: "Todos os itens"},
+        {id: 08, itemName: "Gola Polo", obs: "Todos os itens"},
+        {id: 09, itemName: "Grife", obs: "Todos os itens"},
+        {id: 10, itemName: "Lacoste", obs: "Todos os itens"},
+        {id: 11, itemName: "Social", obs: "Todos os itens"},
+        {id: 12, itemName: "Terno", obs: "Todos os itens"},
     ]
 }]);
